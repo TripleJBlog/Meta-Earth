dbc2749d21e3889a225b35e5290cfaa48ed1c86e
Triplejblog <triplej03@naver.com>
2022-04-04 01:39:07 -0700

    edit package.json

--- server/index.js
+++ server/index.js
@@ -16,6 +16,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.js"));
+  // });
+}
+
 const config = require("./config/key");
 const { User } = require("./models/User");
 mongoose
