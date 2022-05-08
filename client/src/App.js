fd979c7920d0d78da826adecd40cfaefddc31667
Triplejblog <triplej03@naver.com>
2022-05-07 23:39:15 -0700

    add navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
 import "./App.css";
-import { render } from "react-dom";
-import { Link } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+function Home() {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home </Link>
+        <Link to="register">Register </Link>
+        <Link to="login">Login </Link>
+        <Link to="dashboard">Dashboard </Link>
+      </nav>
+      <hr />
+      {/* <LandingPage /> */}
+      <Outlet />
+    </>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
+        <Route path="/" element={<Home />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/views/NavBar";
-import RegisterPage from "./components/views/RegisterPage/RegisterPage";
-import LoginPage from "./components/views/LoginPage/LoginPage";
-import LandingPage from "./components/views/LandingPage/LandingPage";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import promiseMiddleware from "redux-promise";
@@ -32,22 +26,3 @@ render(
   </Provider>,
   document.getElementById("root")
 );
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA);
-// render(
-//   <BrowserRouter>
-//     <Routes>
-//       <Route path="/" element={<LandingPage />} />
-//       <Route path="login" element={<LoginPage />} />
-//       <Route path="register" element={<RegisterPage />} />
-//       <Route path="app" element={<App />} />
-//     </Routes>
-//   </BrowserRouter>,
-//   document.getElementById("root")
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
